95e5f7be33c4d34c80482bf1d784abb9114ed353
Zdenek <zdendaku@gmail.com>
2026-07-12 19:29:21 +0200

    chore: baseline import to GitHub (issue 107)

---
+++ index.ts
@@ -0,0 +1,9 @@
+/*
+ *  This file is part of CassandraGargoyle Community Project
+ *  Licensed under the MIT License - see LICENSE file for details
+ */
+
+// Public exports for the Pilot open panel
+// Re-exports the standalone panel that opens files/workspaces in the Electron host
+
+export { OpenPanel } from './OpenPanel';
