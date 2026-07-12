17aa60319f97074b62e2e6713cc4513850088961
Zdenek <zdendaku@gmail.com>
2026-07-12 19:29:21 +0200

    chore: baseline import to GitHub (issue 107)

---
+++ index.ts
@@ -0,0 +1,9 @@
+/*
+ *  This file is part of CassandraGargoyle Community Project
+ *  Licensed under the MIT License - see LICENSE file for details
+ */
+
+// Public entry point for the shared NodeTablePanel component
+
+export { NodeTablePanel } from './NodeTablePanel';
+export type { TableFilter, NodeTableRow } from './NodeTablePanel';
