3724b5a017e57be4a554415167623efbb81dedf7
Zdenek <zdendaku@gmail.com>
2026-07-12 19:29:21 +0200

    chore: baseline import to GitHub (issue 107)

---
+++ index.ts
@@ -0,0 +1,10 @@
+/*
+ *  This file is part of CassandraGargoyle Community Project
+ *  Licensed under the MIT License - see LICENSE file for details
+ */
+
+// Public API for the Carousel component
+// Re-exports the main component and types
+
+export { Carousel } from './Carousel';
+export type { CarouselItem, CarouselProps } from './Carousel';
